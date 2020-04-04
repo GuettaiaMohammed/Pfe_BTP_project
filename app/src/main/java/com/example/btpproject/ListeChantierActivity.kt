92c1875df7148913c897545cb0555f5514252241
Guettaia Mohammed <guettaia8@gmail.com>
2020-04-04 17:25:37 +0200

    commit 11: liste des chantier, début.

---
+++ app/src/main/java/com/example/btpproject/ListeChantierActivity.kt
@@ -0,0 +1,12 @@
+package com.example.btpproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListeChantierActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liste_chantier)
+    }
+}
