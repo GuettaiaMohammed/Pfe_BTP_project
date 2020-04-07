a0501abc6268507071a5edc80eface53e1a5fdbe
ikram <ikramb542@gmail.com>
2020-04-07 14:18:52 +0100

    modificationBrahmi

---
+++ app/src/main/java/com/example/btpproject/AjouterSuiviActivity.kt
@@ -0,0 +1,13 @@
+package com.example.btpproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AjouterSuiviActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ajouter_qte_realise)
+
+    }
+}
---
+++ app/src/main/java/com/example/btpproject/Notification.kt
@@ -0,0 +1,29 @@
+package com.example.btpproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_action_item_layout.*
+
+class Notification : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.custom_action_item_layout)
+
+
+    }
+
+
+
+    private fun showAlert() {
+        //INSTANTIATE ALERTDIALOG BUILDER
+
+    }
+
+
+}
